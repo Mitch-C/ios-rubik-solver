073aef6bf18a729139c0b678b2640ccefbf599e7
Mitchell Coad <mitchellcoad@Mitchells-Mac-mini.local>
2017-11-22 15:18:48 +1300

    Create Swift sub project.

---
+++ Swift/RubikSolver-iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RubikSolver-iOS
+//
+//  Created by rhcpfan on 15/01/17.
+//  Copyright © 2017 HomeApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
